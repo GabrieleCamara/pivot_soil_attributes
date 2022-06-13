18a0e9d9e2bf51f32f93ec18f2e5aa5d9c69364d
Gabriele Camara <36965321+GabrieleCamara@users.noreply.github.com>
2022-06-13 08:45:19 -0300

    Organizing the comments

--- clay_multi_pivot.py
+++ clay_multi_pivot.py
@@ -1,18 +1,18 @@
+# -- Adding layers -- #
 # Vector layer
 vlayer = iface.activeLayer()
-
-# -- Adding stats mean to vector layer -- #
-# Adding a new column in attribute table
-provider = vlayer.dataProvider()
-raster_mean = QgsField("clay_mean", QVariant.Double)
-provider.addAttributes([raster_mean])
-vlayer.updateFields()
 
 # Raster layer
 fn = 'C:/Users/EHDGZ/outlier_detection/br_clay_content_0-5cm_pred_g_kg.tif'
 rlayer = iface.addRasterLayer(fn, '')
 
-#Variar pivot id para selecionar diferentes feições
+# -- Adding a new column in attribute table of vlayer to receive stats mean of raster -- #
+provider = vlayer.dataProvider()
+raster_mean = QgsField("clay_mean", QVariant.Double)
+provider.addAttributes([raster_mean])
+vlayer.updateFields()
+
+# -- Going through the pivots, each pivot is a mask to clip the raster -- #
 count_pivot = 1
 while count_pivot <= 21040:
     vlayer.selectByExpression('"PIVOTID"=\'%s\'' %count_pivot,QgsVectorLayer.SetSelection)
@@ -23,7 +23,7 @@ while count_pivot <= 21040:
     pr = vl.dataProvider()
     pr.addFeatures(vlayer.selectedFeatures())
     vl.updateExtents()
-#    QgsProject.instance().addMapLayer(vl)
+    # QgsProject.instance().addMapLayer(vl)
     alg_params = {
         'ALPHA_BAND': False,
         'CROP_TO_CUTLINE': True,
@@ -42,24 +42,25 @@ while count_pivot <= 21040:
         'Y_RESOLUTION': None,
         'OUTPUT': 'TEMPORARY_OUTPUT'
     }
-    # Processing Clip raster by mask
+    # Processing clip raster by mask
     result = processing.run('gdal:cliprasterbymasklayer', alg_params)
     # Creating new raster layer
     new_rlayer = QgsRasterLayer(result['OUTPUT'], 'OutputFile_%s' %count_pivot)
     # Adding the new raster layer in QGIS
-#    QgsProject.instance().addMapLayer(new_rlayer)
+    # QgsProject.instance().addMapLayer(new_rlayer)
     
-#    # -- Mean of a raster attributes -- #
+    # -- Mean of a raster attributes -- #
     stats = new_rlayer.dataProvider().bandStatistics(1, QgsRasterBandStats.All)
     stats_mean = stats.mean
     print(stats_mean)
 
-    # Updating the new field for each feature
+    # -- Updating the new field for each feature -- #
     idx = provider.fieldNameIndex('clay_mean')
     for feature in selection:
         attrs = {idx : float(stats_mean)}
         vlayer.dataProvider().changeAttributeValues({feature.id() : attrs})
     
     count_pivot += 1
-    
+
+# Removes the last selection of vlayer 
 vlayer.removeSelection()
